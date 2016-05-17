1a28e1fa1cb852013d9f128b9aa58df948d9cfc4
cymon343 <cymon343@hotmail.com>
2016-05-17 12:34:12 +0200

    ReBuilt The ControllerLayer

--- MainSKI/ControllerLayer/Properties/AssemblyInfo.cs
+++ MainSKI/ControllerLayer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ea88122f-4e3c-4abe-abb1-54cc394ca558")]
+[assembly: Guid("55a205d8-948f-4168-bca7-adc8c10e35e1")]
 
 // Version information for an assembly consists of the following four values:
 //
